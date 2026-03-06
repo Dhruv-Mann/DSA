65db4e79772265e0b9ab9669f70bf9b9837cb233
Dhruv-Mann <dhruvmann139@gmail.com>
2026-03-06 17:43:50 +0530

    Added Longest Consecutive Sequence solution

---
+++ Leetcodes-Python/128_Longest_Consecutive_Sequence.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Convert list to set for O(1) lookup
+        numSet = set(nums)
+        longest = 0
+        
+        for n in numSet:
+            # Only start counting from sequence beginnings
+            # (numbers where n-1 does not exist)
+            if (n - 1) not in numSet:
+                length = 0
+                # Count consecutive numbers from this starting point
+                while (n + length) in numSet:
+                    length += 1
+                # Update longest sequence found so far
+                longest = max(length, longest)
+        
+        return longest
