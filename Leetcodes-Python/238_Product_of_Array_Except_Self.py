af1c722ddd1b0b6006bf914dcb257483cb77ad09
Dhruv-Mann <dhruvmann139@gmail.com>
2026-03-06 17:45:36 +0530

    Add comments to Product of Array Except Self solution

--- Leetcodes-Python/238_Product_of_Array_Except_Self.py
+++ Leetcodes-Python/238_Product_of_Array_Except_Self.py
@@ -4,13 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        res = [1]* len(nums)
+        # Initialize result array with all 1s
+        res = [1] * len(nums)
+        
+        # First pass: fill result with product of all elements to the left
+        # For each position i, res[i] = product of all nums[0] to nums[i-1]
         prefix = 1
         for i in range(len(nums)):
             res[i] = prefix
+            # Update prefix for next iteration
             prefix *= nums[i]
+        
+        # Second pass: multiply result by product of all elements to the right
+        # For each position i, res[i] *= product of all nums[i+1] to nums[n-1]
         postfix = 1
-        for i in range(len(nums)-1 , -1, -1):
-            res[i]*= postfix
+        for i in range(len(nums) - 1, -1, -1):
+            # Multiply by postfix (product of all elements to the right)
+            res[i] *= postfix
+            # Update postfix for next iteration (going right to left)
             postfix *= nums[i]
+        
+        # Result now contains product of all elements except self at each index
         return res
