dfa21a27cdd92199828ae6aa8e550a2bf86e7a94
Dhruv-Mann <dhruvmann139@gmail.com>
2026-03-07 10:38:58 +0530

    Added Valid Palindrome solution

---
+++ Leetcodes-Python/125_Valid_Palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # Two-pointer check from both ends of the string.
+        L = 0
+        R = len(s) - 1
+        while L < R:
+            # Skip characters that are not letters/digits.
+            while L < R and not s[L].isalnum():
+                L += 1
+            while L < R and not s[R].isalnum():
+                R -= 1
+
+            # Compare case-insensitively.
+            if s[L].lower() != s[R].lower():
+                return False
+
+            L += 1
+            R -= 1
+
+        return True
