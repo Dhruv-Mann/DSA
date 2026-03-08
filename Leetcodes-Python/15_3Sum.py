6c88e2cccb72410436e1d395e882fb4aac4ebd89
Dhruv-Mann <dhruvmann139@gmail.com>
2026-03-08 12:03:40 +0530

    Added 3Sum solution

---
+++ Leetcodes-Python/15_3Sum.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        # Sort array to enable two-pointer approach and skip duplicates
+        nums.sort()
+        
+        # Fix the first number and find two numbers that sum to -first
+        for i, a in enumerate(nums):
+            # Skip duplicate first numbers to avoid duplicate triplets
+            if i > 0 and a == nums[i - 1]:
+                continue
+            
+            # Two-pointer approach for the remaining two numbers
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                currentSum = a + nums[l] + nums[r]
+                
+                # Adjust pointers based on sum comparison
+                if currentSum > 0:
+                    r -= 1
+                elif currentSum < 0:
+                    l += 1
+                else:
+                    # Found a valid triplet
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    
+                    # Skip duplicate second numbers
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        
+        return res
+
+           
+       
+
+        
